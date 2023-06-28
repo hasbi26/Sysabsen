a627a71334572a2e3a39b37149bcbba95e73868d
Hasbi <hasbisimaulansyah@gmail.com>
2023-06-28 16:03:34 +0700

    first commit

---
+++ application/views/template/footer.php
@@ -0,0 +1,6 @@
+<footer class="main-footer">
+    <div class="pull-right hidden-xs">
+        <b>Version</b> 2.3.0
+    </div>
+    <strong> &copy; 2023 <a href="#">Absensi SMK Muhammadiyah 2 Cibiru Bandung</a>.</strong> Template by Nanda Handayani
+</footer>
